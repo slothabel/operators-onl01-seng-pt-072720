4a5607cfb9abb1f7d37e1f222bfe3fad15dce4b2
Yoshi Mejia <yoshimejia00@gmail.com>
2020-07-31 03:01:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,12 @@
 def unsafe?(speed)
 if speed > 60 && speed < 40
 	true
-else speed > 40 && < 60
+else speed > 40 && speed < 60
 	false
-
-
 end
 end
 
 
-def not_safe?(speed)
-speed > 40 ? true : false && speed > 60 ? true : false
-end
+#def not_safe?(speed)
+#speed > 40 ? true : false && speed > 60 ? true : false
+#end
